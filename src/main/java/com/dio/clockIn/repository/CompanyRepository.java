69ee258d72dcde7757698c7409ce8ad48273491c
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-31 19:04:50 -0300

    Created Company Repository

---
+++ src/main/java/com/dio/clockIn/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.dio.clockIn.repository;
+
+import com.dio.clockIn.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
