b4c84f2560a7d742914d052184accb97ca6e1840
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-31 20:50:08 -0300

    Solved Location Service Object instead Location Class problem

---
+++ src/main/java/com/dio/clockIn/service/LocationService.java
@@ -0,0 +1,38 @@
+package com.dio.clockIn.service;
+
+
+import com.dio.clockIn.model.Location;
+import com.dio.clockIn.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LocationService {
+    LocationRepository locationRepository;
+
+    @Autowired
+    public LocationService(LocationRepository locationRepository){
+        this.locationRepository = locationRepository;
+    }
+
+    public Location saveLocation(Location location){
+        return locationRepository.save(location);
+    }
+
+    public void deleteLocation(Long id){
+        locationRepository.deleteById(id);
+    }
+
+    public List<Location> listLocation(){
+        return locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocationById(Long id){
+        return locationRepository.findById(id);
+    }
+
+    public Location updateLocation(Location location){
+        return locationRepository.save(location);
+    }
+}
