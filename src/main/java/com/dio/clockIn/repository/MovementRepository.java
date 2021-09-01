331085bfa619fb6dbf3f40442c0ff9b3567c550a
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-31 22:06:43 -0300

    Created Movement Repository

---
+++ src/main/java/com/dio/clockIn/repository/MovementRepository.java
@@ -0,0 +1,9 @@
+package com.dio.clockIn.repository;
+
+import com.dio.clockIn.model.Movement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovementRepository extends JpaRepository<Movement,Long> {
+}
