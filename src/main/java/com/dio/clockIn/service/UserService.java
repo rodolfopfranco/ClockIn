744834a89fa4ea4435f2c65fb4ec2db49b09a087
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-31 21:05:11 -0300

    Created User Service

--- src/main/java/com/dio/clockIn/service/UserService.java
+++ src/main/java/com/dio/clockIn/service/UserService.java
@@ -1,4 +1,40 @@
 package com.dio.clockIn.service;
 
+import com.dio.clockIn.model.User;
+import com.dio.clockIn.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public List<User> listUser(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(User user){
+        return userRepository.save(user);
+    }
+
 }
