28b6aeee4f4836378c1f46045cb3473fac041ab7
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-31 21:36:42 -0300

    Created Event Repository

---
+++ src/main/java/com/dio/clockIn/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.dio.clockIn.repository;
+
+import com.dio.clockIn.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
